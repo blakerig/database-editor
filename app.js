c9e828a7666a699fce01978b9053c182bee04efa
blakerig <blakerig2@gmail.com>
2026-01-31 12:52:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
