94523c49b17b3506f803756531cbc203e10f3b71
blakerig <blakerig2@gmail.com>
2026-01-31 12:57:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ WHERE id=$12
 } else {
 await pool.query(`
 INSERT INTO destinations (name, flag, main_image, currency, official_languages, timezone, capital, category, description, status, sections)
-VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)
+VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)
 `, [name, flag, main_image, currency, official_languages, timezone, capital, category, description, status, sections]);
 }
 
